60804e577e8db815a962ebe41b3b526ab3b7b7e1
Oka <110789617+ocarina0001@users.noreply.github.com>
2022-10-11 16:38:02 -0400

    Add files via upload

---
+++ MilkingMachine/Custom_Genital_Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using rjw;
+
+namespace MilkingMachine
+{
+    public static class Custom_Genital_Helper
+    {
+		public static bool is_penis(Hediff hed)
+		{
+			if (hed == null)
+			{
+				return false;
+			}
+			if (!hed.def.defName.ToLower().Contains("penis") && !hed.def.defName.ToLower().Contains("ovipositorm") && !hed.def.defName.ToLower().Contains("pegdick"))
+			{
+				if (hed.def.defName.ToLower().Contains("tentacle"))
+				{
+					return !hed.def.defName.ToLower().Contains("penis");
+				}
+				return false;
+			}
+			return true;
+		}
+		public static bool is_breast(Hediff hed)
+		{
+			if (hed == null)
+			{
+				return false;
+			}
+			if (!hed.def.defName.ToLower().Contains("breast") && !hed.def.defName.ToLower().Contains("chest"))
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ MilkingMachine/MilkingHediff.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using rjw;
+
+namespace MilkingMachine
+{
+    public class MilkingHediff : HediffWithComps
+    {
+        public static float penisSize;
+        public static float breastSize;
+        public override void Tick()
+        {
+            Pawn pawn = this.pawn;
+            if (pawn.IsHashIntervalTick(60000))
+            {
+                if (pawn.IsColonist || pawn.IsPrisoner || pawn.IsSlave)
+                {
+                    IEnumerable<Hediff> penises = pawn.GetGenitalsList().Where(genitalHediff => Custom_Genital_Helper.is_penis(genitalHediff));
+                    bool hasPenis = !penises.EnumerableNullOrEmpty();
+                    IEnumerable<Hediff> breasts = pawn.GetBreastList().Where(breastHediff => Custom_Genital_Helper.is_breast(breastHediff));
+                    bool hasBreast = !breasts.EnumerableNullOrEmpty();
+                    if (hasPenis)
+                    {
+                        foreach (Hediff penis in penises)
+                        {
+                            CompHediffBodyPart rjwPenisHediff = penis.TryGetComp<CompHediffBodyPart>();
+                            if (rjwPenisHediff != null)
+                            {
+                                PartSizeExtension.TryGetLength(penis, out float penisLength);
+                                PartSizeExtension.TryGetGirth(penis, out float penisGirth);
+                                penisSize = (penisGirth + penisLength) / 2;
+                                if (penisSize < 1)
+                                    penisSize = 1;
+                                // Log.Message(pawn + "'s penis size is: " + penisSize);
+                                Thing penisThing = ThingMaker.MakeThing(ThingDefOf.UsedCondom);
+                                penisThing.stackCount = (int)(pawn.BodySize * penisSize);
+                                GenPlace.TryPlaceThing(penisThing, pawn.Position, pawn.Map, ThingPlaceMode.Near);
+                            }
+                        }
+                    }
+                    if (hasBreast)
+                    {
+                        foreach (Hediff breast in breasts)
+                        {
+                            CompHediffBodyPart rjwBreastHediff = breast.TryGetComp<CompHediffBodyPart>();
+                            if (rjwBreastHediff != null)
+                            {
+                                PartSizeExtension.TryGetBreastWeight(breast, out float breastWeight);
+                                PartSizeExtension.TryGetCupSize(breast, out float cupSize);
+                                breastSize = breastWeight + cupSize;
+                                if (breastSize < 1)
+                                    breastSize = 1;
+                                // Log.Message(pawn + "'s breast size is: " + breastSize);
+                                Thing breastThing = ThingMaker.MakeThing(ThingDefOf.Milk);
+                                breastThing.stackCount = (int)(pawn.BodySize * breastSize);
+                                GenPlace.TryPlaceThing(breastThing, pawn.Position, pawn.Map, ThingPlaceMode.Near);
+                            }
+                        }
+                    }
+                }
+                else
+                    return;
+            }
+        }
+	}
+}
